5cdafd397c6c4e380e9342df31a3252584c8e18a
Edwin Camacho <edwin.a.camacho@gmail.com>
2022-09-09 13:48:30 +0100

    Se corrige nombre de nomenclatura de temperaturas

--- src/OpcionesConversion/OpcionesConversionTemperatura.java
+++ src/OpcionesConversion/OpcionesConversionTemperatura.java
@@ -12,11 +12,11 @@ public class OpcionesConversionTemperatura {
 			String opciones = JOptionPane.showInputDialog(null, "Elija una Opción", "Menú", 
 					JOptionPane.PLAIN_MESSAGE, null, new Object[] 
 					{	"Grados Celsius a Grados Fahrenheit", 
-						"Grados Celsius a Grados Kelvi",
-						"Grados Fahrenhei a Grados Celsius",
-						"Grados Fahrenhei a Grados Kelvi",
-						"Grados Kelvi a Grados Celsius",
-						"Grados Kelvi a Grados Fahrenhei"
+						"Grados Celsius a Grados Kelvin",
+						"Grados Fahrenheit a Grados Celsius",
+						"Grados Fahrenheit a Grados Kelvin",
+						"Grados Kelvin a Grados Celsius",
+						"Grados Kelvin a Grados Fahrenheit"
 					},
 				"Selección").toString();
 			switch(opciones) {
@@ -24,23 +24,23 @@ public class OpcionesConversionTemperatura {
 				temperatura.ConvertirTemperatura_Celsius_Fahrenheit(valor);
 				break;
 			
-			case "Grados Celsius a Grados Kelvi":
+			case "Grados Celsius a Grados Kelvin":
 				temperatura.ConvertirTemperatura_Celsius_Kelvin(valor);
 				break;
 				
-			case "Grados Fahrenhei a Grados Celsius":
+			case "Grados Fahrenheit a Grados Celsius":
 				temperatura.ConvertirTemperatura_Fahrenheit_Celsius(valor);
 				break;
 				
-			case "Grados Fahrenhei a Grados Kelvi":
+			case "Grados Fahrenheit a Grados Kelvin":
 				temperatura.ConvertirTemperatura_Fahrenheit_Kelvin(valor);
 				break;
 				
-			case "Grados Kelvi a Grados Celsius":
+			case "Grados Kelvin a Grados Celsius":
 				temperatura.ConvertirTemperatura_Kelvin_Celsius(valor);
 				break;
 				
-			case "Grados Kelvi a Grados Fahrenhei":
+			case "Grados Kelvin a Grados Fahrenheit":
 				temperatura.ConvertirTemperatura_Kelvin_Fahrenheit(valor);
 				break;
 				
